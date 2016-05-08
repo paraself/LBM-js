f809858cd6763ae1439a29b947a90fae05b32e67
mrjleo <mr.ljleo@gmail.com>
2016-05-08 19:21:50 +0200

    add slider for steps per frame

--- src/js/control.js
+++ src/js/control.js
@@ -4,9 +4,13 @@ var lbm_btn_run;
 var lbm_btn_stop;
 var lbm_btn_continue;
 var lbm_select;
+var lbm_range_steps;
+var lbm_span_steps;
 
 var GRID_SIZE_X = 100;
 var GRID_SIZE_Y = 100;
+var STEPS = 10;
+var MAX_STEPS = 100;
 var SCALE = 3;
 
 var SCENARIOS = {
@@ -22,6 +26,13 @@ function on_body_load() {
 	lbm_btn_run = document.getElementById("lbm_btn_run");
 	lbm_btn_stop = document.getElementById("lbm_btn_stop");
 	lbm_btn_continue = document.getElementById("lbm_btn_continue");
+
+	lbm_range_steps = document.getElementById("lbm_range_steps");
+	lbm_range_steps.min = 1;
+	lbm_range_steps.max = MAX_STEPS;
+	lbm_range_steps.value = STEPS;
+	lbm_span_steps = document.getElementById("lbm_span_steps");
+	lbm_span_steps.textContent = STEPS;
 
 	lbm_canvas = document.getElementById("lbm_canvas");
 	lbm_canvas.setAttribute("height", GRID_SIZE_Y * SCALE + "px");
@@ -46,7 +57,7 @@ function on_body_load() {
 	// initialize worker
 	lbm_worker = new Worker("js/lbm.min.js");
 	lbm_worker.onmessage = worker_message;
-	set_lbm_options({cols: GRID_SIZE_X, rows: GRID_SIZE_Y});
+	set_lbm_options({cols: GRID_SIZE_X, rows: GRID_SIZE_Y, steps_per_frame: STEPS});
 
 	draw_init(GRID_SIZE_X, GRID_SIZE_Y, SCALE);
 }
@@ -57,6 +68,11 @@ function worker_command(cmd, value) {
 
 function set_lbm_options(options) {
 	worker_command("set", options);
+}
+
+function lbm_steps_changed() {
+	set_lbm_options({steps_per_frame: lbm_range_steps.value});
+	lbm_span_steps.textContent = Number(lbm_range_steps.value);
 }
 
 function lbm_run() {
